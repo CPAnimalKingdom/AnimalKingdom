fb285f20102f6a4c5a7318e896dd24c74fc1decc
Dan Ndombe <dndombe@gmail.com>
2017-10-13 21:03:37 -0700

    Created bundle, animal and settings models

---
+++ AnimalKingdom/Models/Animal.swift
@@ -0,0 +1,52 @@
+//
+//  Animal.swift
+//  AnimalKingdom
+//
+//  Created by Dan on 10/13/17.
+//  Copyright © 2017 Dan. All rights reserved.
+//
+
+import UIKit
+
+class Animal: NSObject {
+    var commonName: NSString
+    var scientificName: NSString
+    var type: NSString
+    var diet: NSString
+    var groupName: NSString
+    var lifeSpan: NSString
+    var size: NSString
+    var weight: NSString
+    var mainPhoto: NSString
+    var thumbnailPhoto: NSString
+    var relativeSize: NSString
+    var about: NSDictionary
+    var facts: [NSString]
+
+    init(dictionary: NSDictionary) {
+        self.commonName = dictionary["commonName"] as! NSString
+        self.scientificName = dictionary["scientificName"] as! NSString
+        self.type = dictionary["type"] as! NSString
+        self.diet = dictionary["diet"]  as! NSString
+        self.groupName = dictionary["groupName"] as! NSString
+        self.lifeSpan = dictionary["lifeSpan"] as! NSString
+        self.size = dictionary["size"] as! NSString
+        self.weight = dictionary["weight"] as! NSString
+        self.mainPhoto = dictionary["mainPhoto"] as! NSString
+        self.thumbnailPhoto = dictionary["thumbnail"] as! NSString
+        self.relativeSize = dictionary["relativeSize"] as! NSString
+        self.about = dictionary["about"] as! NSDictionary
+        self.facts = dictionary["facts"] as! [NSString]
+    }
+    
+    class func animalArray(dictionaries: [NSDictionary]) -> [Animal] {
+        var animals: [Animal] = []
+        
+        for dictionary in dictionaries {
+            let animal = Animal(dictionary: dictionary)
+            animals.append(animal)
+        }
+        return animals
+    }
+
+}
---
+++ AnimalKingdom/Models/Settings.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsManager.swift
+//  AnimalKingdom
+//
+//  Created by Dan on 10/13/17.
+//  Copyright © 2017 Dan. All rights reserved.
+//
+
+import UIKit
+
+class Settings: NSObject {
+
+}

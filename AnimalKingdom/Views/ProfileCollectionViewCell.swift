7aa513b8cedaa4f9d8ff4f3ebcb3e8f133642708
Aabeeya Salman <git@aabeeya.com>
2017-10-22 02:11:59 -0700

    Add profile view for the logged in user's content

---
+++ AnimalKingdom/Views/ProfileCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileCollectionViewCell.swift
+//  AnimalKingdom
+//
+//  Created by Aabeeya on 10/22/17.
+//  Copyright © 2017 Dan. All rights reserved.
+//
+
+import UIKit
+
+class ProfileCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var animalImage: UIImageView!
+}

1610f2ffa5a1fcca7578c81110a8570efbe4b8a5
Aabeeya Salman <git@aabeeya.com>
2017-10-22 17:07:19 -0700

    Fix spacing on UICollection

--- AnimalKingdom/View Controllers/ProfileViewController.swift
+++ AnimalKingdom/View Controllers/ProfileViewController.swift
@@ -86,5 +86,12 @@ class ProfileViewController: UIViewController, UICollectionViewDataSource, UICol
         }
         return cell
     }
-
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 1
+    }
 }

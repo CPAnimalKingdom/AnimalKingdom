6abe7cda23150c26c624eff8200a9d91afd7d696
NNeyzi <naderneyzi@gmail.com>
2017-10-22 20:12:56 -0700

    movement, interaction, animation

--- AnimalKingdom/Models/ARPlane.swift
+++ AnimalKingdom/Models/ARPlane.swift
@@ -11,31 +11,29 @@ import ARKit
 
 class ARPlane: SCNNode {
 
-    var anchor: ARPlaneAnchor!
+    var planeAnchor: ARPlaneAnchor!
     var planeGeometry: SCNPlane!
 
-    init(anchor: ARPlaneAnchor) {
+    init(_ planeAnchor: ARPlaneAnchor) {
         super.init()
 
-        self.anchor = anchor
-        planeGeometry = SCNPlane(width: CGFloat(anchor.extent.x), height: CGFloat(anchor.extent.z))
+        self.planeAnchor = planeAnchor
+        planeGeometry = SCNPlane(width: CGFloat(planeAnchor.extent.x), height: CGFloat(planeAnchor.extent.z))
 
         let material = SCNMaterial()
         material.diffuse.contents = UIImage(named: "tron_grid")
-
         planeGeometry.materials = [material]
 
         let planeNode = SCNNode(geometry: planeGeometry)
-        planeNode.position = SCNVector3Make(anchor.center.x, 0, anchor.center.z)
+        planeNode.position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z)
         // Planes in SceneKit are vertical by default so we need to rotate 90degrees
         // to match planes in ARKit
-        planeNode.transform = SCNMatrix4MakeRotation(-Float.pi / 2.0, 1.0, 0.0, 0.0);
+        planeNode.eulerAngles.x = -.pi / 2
 
         setTextureScale()
         addChildNode(planeNode)
     }
 
-    // Will never be used
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -44,10 +42,6 @@ class ARPlane: SCNNode {
         let width = planeGeometry.width
         let height = planeGeometry.height
 
-        // As the width/height of the plane updates, the material should
-        // cover the entire plane, repeating the texture over and over. Also if the
-        // grid is less than 1 unit, don't squash the texture to fit, so
-        // scaling updates the texture co-ordinates to crop the texture in that case
         if let material = planeGeometry.materials.first {
             material.diffuse.contentsTransform = SCNMatrix4MakeScale(Float(width), Float(height), 1)
             material.diffuse.wrapS = SCNWrapMode.repeat
@@ -55,15 +49,11 @@ class ARPlane: SCNNode {
         }
     }
 
-    func update(anchor: ARPlaneAnchor) {
-        planeGeometry.width = CGFloat(anchor.extent.x);
-        planeGeometry.height = CGFloat(anchor.extent.z);
+    func update(planeAnchor: ARPlaneAnchor) {
+        planeGeometry.width = CGFloat(planeAnchor.extent.x);
+        planeGeometry.height = CGFloat(planeAnchor.extent.z);
 
-        // When the plane is first created it's center is 0,0,0 and the nodes
-        // transform contains the translation parameters. As the plane is updated
-        // the planes translation remains the same but it's center is updated so
-        // we need to update the 3D geometry position
-        position = SCNVector3Make(anchor.center.x, 0, anchor.center.z);
+        position = SCNVector3Make(planeAnchor.center.x, 0, planeAnchor.center.z);
         setTextureScale()
     }
 }
--- AnimalKingdom/View Controllers/ARViewController.swift
+++ AnimalKingdom/View Controllers/ARViewController.swift
@@ -16,6 +16,8 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
     var animations = [String: CAAnimation]()
     var planes = [UUID: ARPlane]()
     var idle: Bool = true
+    var modelNode: SCNNode?
+    let walkKey = "walk"
 
 
     // MARK: Lifecycle
@@ -25,42 +27,36 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
 
         sceneView.delegate = self
         sceneView.showsStatistics = true
-//        sceneView.autoenablesDefaultLighting = true
-        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+//        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
 
-        setupScene()
+        // TODO: Lighting
+        sceneView.autoenablesDefaultLighting = false
+
+        let scene = SCNScene()
+        sceneView.scene = scene
+
+        UIApplication.shared.isIdleTimerDisabled = true
         loadAnimations()
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewDidAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        setupSession()
+        let configuration = ARWorldTrackingConfiguration()
+        configuration.planeDetection = .horizontal
+        sceneView.session.run(configuration)
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
 
         sceneView.session.pause()
+        UIApplication.shared.isIdleTimerDisabled = false
     }
 
     // MARK: Helpers
 
-    func setupScene() {
-        let scene = SCNScene()
-        sceneView.scene = scene
-
-    }
-
-    func setupSession() {
-        let configuration = ARWorldTrackingConfiguration()
-
-        configuration.planeDetection = .horizontal
-
-        sceneView.session.run(configuration)
-    }
-
-    func loadAnimations () {
+    func insertModel(position: SCNVector3 = SCNVector3(0, -1, -2), scale: SCNVector3 = SCNVector3(0.05, 0.05, 0.05)) {
         let idleScene = SCNScene(named: "art.scnassets/Elephant.dae")!
 
         let node = SCNNode()
@@ -68,73 +64,155 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
             node.addChildNode(child)
         }
 
-        node.position = SCNVector3(0, -1, -2)
-        node.scale = SCNVector3(0.2, 0.2, 0.2)
+        node.position = position
+        node.scale = scale
 
         sceneView.scene.rootNode.addChildNode(node)
-
-        loadAnimation(withKey: "dancing", sceneName: "art.scnassets/testi", animationIdentifier: "testi-1")
+        modelNode = node
     }
 
-    func loadAnimation(withKey: String, sceneName:String, animationIdentifier:String) {
+    func loadAnimations() {
+        //Walk
+        let startWalk = 25.5666667064031
+        let endWalk = 27.5666667064031
+        loadAnimation(key: walkKey, startTime: startWalk, endTime: endWalk)
+    }
+
+    func loadAnimation(key: String, startTime: Double, endTime: Double) {
+        let sceneName = "art.scnassets/Elephant9Animations"
+        let animationIdentifier = "Elephant9Animations-1"
+
         let sceneURL = Bundle.main.url(forResource: sceneName, withExtension: "dae")
         let sceneSource = SCNSceneSource(url: sceneURL!, options: nil)
 
-        if let animationObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self) {
-            animationObject.repeatCount = 1
-            animationObject.fadeInDuration = CGFloat(1)
-            animationObject.fadeOutDuration = CGFloat(0.5)
+        if let animationsObject = sceneSource?.entryWithIdentifier(animationIdentifier, withClass: CAAnimation.self) {
+            let animation = CAAnimationGroup()
+            let sub = animationsObject.copy() as! CAAnimation
+            sub.timeOffset = 25.5666667064031
+            animation.animations = [sub]
+            animation.duration = 27.5666667064031 - sub.timeOffset
+            animation.repeatCount = Float.greatestFiniteMagnitude
 
-            animations[withKey] = animationObject
+            animations[key] = animation
         }
     }
 
     func playAnimation(key: String) {
-        // Add the animation to start playing it right away
         sceneView.scene.rootNode.addAnimation(animations[key]!, forKey: key)
     }
 
     func stopAnimation(key: String) {
-        // Stop the animation with a smooth transition
         sceneView.scene.rootNode.removeAnimation(forKey: key, blendOutDuration: CGFloat(0.5))
     }
 
-    // MARK: GestureRecognizer
+    // MARK: Animation
 
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        let location = touches.first!.location(in: sceneView)
+    var isWalking = false
 
-        // Let's test if a 3D Object was touch
-        var hitTestOptions = [SCNHitTestOption: Any]()
-        hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+    func toggleWalk() {
+        guard let modelNode = modelNode else { return }
 
-        let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+        if !isWalking {
+            playAnimation(key: walkKey)
 
-        if hitResults.first != nil {
-            if(idle) {
-                playAnimation(key: "dancing")
-            } else {
-                stopAnimation(key: "dancing")
+            let walkAction = SCNAction.move(to: SCNVector3Make(modelNode.position.x, modelNode.position.y, modelNode.position.z + 1), duration: 2.0)
+            modelNode.runAction(walkAction, forKey: walkKey)
+
+            isWalking = true
+        } else {
+            stopAnimation(key: walkKey)
+
+            modelNode.removeAction(forKey: walkKey)
+
+            isWalking = false
+        }
+    }
+
+    // MARK: Interaction
+
+    @IBAction func onTap(_ tap: UITapGestureRecognizer) {
+        guard tap.state == .ended else { return }
+
+        let location = tap.location(in: sceneView)
+
+        if modelNode == nil {
+            let hitPlanes = sceneView.hitTest(location, types: .existingPlane)
+
+            if let firstPlane = hitPlanes.first {
+                let planeWorldTransform = firstPlane.worldTransform.columns.3
+                let position = SCNVector3Make(planeWorldTransform.x, planeWorldTransform.y, planeWorldTransform.z)
+
+                insertModel(position: position)
             }
-            idle = !idle
-            return
+        } else {
+            // Test if 3D Object was touch
+            var hitTestOptions = [SCNHitTestOption: Any]()
+            hitTestOptions[SCNHitTestOption.boundingBoxOnly] = true
+
+            let hitResults: [SCNHitTestResult]  = sceneView.hitTest(location, options: hitTestOptions)
+
+            if hitResults.first != nil {
+                if(idle) {
+                    playAnimation(key: "Walk")
+                } else {
+                    stopAnimation(key: "Walk")
+                }
+                idle = !idle
+                return
+            }
+        }
+    }
+
+    @IBAction func onPinch(_ pinch: UIPinchGestureRecognizer) {
+        guard let modelNode = modelNode else { return }
+
+        if pinch.state == .changed {
+            let pinchScaleX = Float(pinch.scale) * modelNode.scale.x
+            let pinchScaleY = Float(pinch.scale) * modelNode.scale.y
+            let pinchScaleZ = Float(pinch.scale) * modelNode.scale.z
+            modelNode.scale = SCNVector3Make(pinchScaleX, pinchScaleY, pinchScaleZ)
+
+            pinch.scale = 1
+        }
+    }
+
+    @IBAction func onRotation(_ rotation: UIRotationGestureRecognizer) {
+        guard let modelNode = modelNode else { return }
+
+        if rotation.state == .changed {
+            modelNode.eulerAngles.y -= Float(rotation.rotation)
+
+            rotation.rotation = 0
+        }
+    }
+
+    @IBAction func onPan(_ pan: UIPanGestureRecognizer) {
+        guard let modelNode = modelNode else { return }
+
+        let location = pan.location(in: sceneView)
+
+        let planeHitTestResults = sceneView.hitTest(location, types: .existingPlaneUsingExtent)
+        if let result = planeHitTestResults.first {
+            let planeHitTestPosition = result.worldTransform.translation
+
+            modelNode.simdPosition = planeHitTestPosition
         }
     }
 
     // MARK: - ARSCNViewDelegate
 
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
-        if anchor.isKind(of: ARPlaneAnchor.self) {
-            let plane = ARPlane.init(anchor: anchor as! ARPlaneAnchor)
-            planes[anchor.identifier] = plane
-            node.addChildNode(plane)
-        }
+        guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
+
+        let plane = ARPlane.init(planeAnchor)
+        planes[planeAnchor.identifier] = plane
+        node.addChildNode(plane)
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
-        if let plane = planes[anchor.identifier] {
-            plane.update(anchor: anchor as! ARPlaneAnchor)
-        }
+        guard let plane = planes[anchor.identifier] else { return }
+
+        plane.update(planeAnchor: anchor as! ARPlaneAnchor)
     }
 
     func renderer(_ renderer: SCNSceneRenderer, didRemove node: SCNNode, for anchor: ARAnchor) {
@@ -142,19 +220,23 @@ class ARViewController: UIViewController, ARSCNViewDelegate {
         planes.removeValue(forKey: anchor.identifier)
     }
 
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
 
-    }
+    // MARK: - ARSessionObserver
 
     func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay.
+        //        sessionInfoLabel.text = "Session was interrupted"
     }
 
     func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-
+        // Reset tracking and/or remove existing anchors if consistent tracking is required.
+        //        sessionInfoLabel.text = "Session interruption ended"
+        //        resetTracking()
     }
 
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user.
+        //        sessionInfoLabel.text = "Session failed: \(error.localizedDescription)"
+        //        resetTracking()
+    }
 }

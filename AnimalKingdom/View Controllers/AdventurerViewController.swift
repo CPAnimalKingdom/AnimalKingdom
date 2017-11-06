fb8b07d91fa36012b95a9a43d888b2a2c22da54a
Dan Ndombe <dndombe@gmail.com>
2017-11-06 11:32:08 -0800

    Adventurer controller complete

--- AnimalKingdom/View Controllers/AdventurerViewController.swift
+++ AnimalKingdom/View Controllers/AdventurerViewController.swift
@@ -11,13 +11,19 @@ import Canvas
 
 class AdventurerViewController: UIViewController {
 
+    @IBOutlet var animalName: UILabel!
+    @IBOutlet var animalImage: UIButton!
     @IBOutlet var animationView: CSAnimationView!
+    @IBOutlet var buttonView: UIView!
+    var dummyAdventurerCounter: Int!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        self.dummyAdventurerCounter = 0
+
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
         self.navigationController?.navigationBar.shadowImage = UIImage()
         self.navigationController?.navigationBar.isTranslucent = true
@@ -27,8 +33,30 @@ class AdventurerViewController: UIViewController {
     }
 
     @IBAction func onButton(_ sender: Any) {
+        let image = UIImage(named: "blur") as UIImage?
+        animalImage.setImage(image, for: .normal)
         animationView.startCanvasAnimation()
+        buttonView.startCanvasAnimation()
+        setImage()
     }
+    
+    func setImage() {
+        let delay = DispatchTime.now() + 0.4
+        DispatchQueue.main.asyncAfter(deadline: delay) {
+            if (self.dummyAdventurerCounter < 2)
+            {
+                self.animalName.text = "THE BENGAL TIGER"
+                let image = UIImage(named: "tiger-adventurer") as UIImage?
+                self.animalImage.setImage(image, for: .normal)
+            } else {
+                self.animalName.text = "THE AFRICAN ELEPHANT"
+                let image = UIImage(named: "african-elephant-adventurer") as UIImage?
+                self.animalImage.setImage(image, for: .normal)
+            }
+        }
+        self.dummyAdventurerCounter = self.dummyAdventurerCounter + 1
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.

e65416b4e67dc8f6ba57f3d695d00d6e206573c5
Aabeeya Salman <git@aabeeya.com>
2017-11-08 23:59:47 -0800

    Display Profile Image in the feed

--- AnimalKingdom/Views/PhotoFeedCell.swift
+++ AnimalKingdom/Views/PhotoFeedCell.swift
@@ -18,11 +18,30 @@ class PhotoFeedCell: UITableViewCell {
     @IBOutlet weak var locationTag: UILabel!
 
     var myImage: UIImage? = nil
+    var profileImage: UIImage? = nil
+
+    var user: User? = nil
+
     func populate(post: Post) {
         imageCaption.text = post.imageCaption
         username.text = post.userName
         locationTag.text = post.locationTag
         imageDateTimeStamp.text = post.dateImageTaken
+
+        let docRef = db.collection("users").document(post.userId)
+
+        docRef.getDocument { (document, error) in
+            if let document = document {
+                if (document.exists) {
+                    self.user = User.init(id: post.userId, dictionary: document.data());
+                    self.setProfileImage()
+                } else {
+                    print("Couldn't find user id: " + post.userId)
+                }
+            } else {
+                print("Couldn't find user id: " + post.userId)
+            }
+        }
 
         self.animalImage.image = UIImage(named: "defaultPictureIcon")
         DispatchQueue.global().async {
@@ -54,6 +73,27 @@ class PhotoFeedCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    func setProfileImage() {
+        DispatchQueue.global().async {
+            do {
+                if let imageURL = NSURL(string: (self.user?.profileImageUrl)!) {
+                    if let data = NSData(contentsOf: imageURL as URL) {
+                        self.profileImage = UIImage(data: data as Data)
+                    }
+                }
+            }
+            DispatchQueue.main.async {
+                if self.profileImage != nil {
+                    UIView.transition(with: self.userImage,
+                                      duration: 0.5,
+                                      options: .transitionCrossDissolve,
+                                      animations: { self.userImage.image = self.profileImage },
+                                      completion: nil)
+                }
+            }
+        }
     }
 
 }
